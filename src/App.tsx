9487c7554cb626b8b8ce474a730c20769478b0e0
namannc13 <chawhannaman5@gmail.com>
2025-03-09 00:44:58 +0530

    Minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,8 @@ function App() {
             </div>
             <p className="text-sm leading-7 text-muted-foreground">
               Full Stack Developer who loves turning coffee into elegant code
-              solutions. I'm currently working as a Product Development Intern
+              solutions. I'm currently working as a{" "}
+              <span className="text-primary">Product Development Intern</span>{" "}
               at{" "}
               <a
                 href="https://agnext.com/"
@@ -73,17 +74,26 @@ function App() {
             </h4>
             <div className="flex flex-col gap-4 text-muted-foreground">
               <p className="text-sm leading-7">
-                All I want to do is build websites that help people and that
-                help me grow as a developer
+                All I want to do is{" "}
+                <span className="text-primary">build websites</span> that help
+                people and that help me grow as a developer
               </p>
               <p className="text-sm leading-7">
-                Currently, I'm focused on exploring modern web technologies and
-                trying out new Languages. When I'm not coding, I'm reading about
-                new developments in tech or contributing to open-source projects
+                Currently, I'm focused on{" "}
+                <span className="text-primary">
+                  exploring modern web technologies
+                </span>{" "}
+                and{" "}
+                <span className="text-primary">trying out new Languages</span>.
+                When I'm not coding, I'm reading about new developments in tech
+                or{" "}
+                <span className="text-primary">
+                  contributing to open-source projects
+                </span>
               </p>
               <p className="text-sm leading-7">
-                I love listening to music of all genres. I probably listen to
-                music more than I should{" "}
+                <span className="text-primary">I love listening to music</span>{" "}
+                of all genres. I probably listen to music more than I should{" "}
                 <a
                   href="https://open.spotify.com/playlist/7aJTTUpRfBaptrgC37KpH9"
                   className="relative rounded bg-muted px-[0.75rem] py-[0.2rem] italic text-sm font-semibold text-primary hover:bg-muted/70 w-fit"
@@ -100,7 +110,8 @@ function App() {
             <div className="flex flex-col gap-4">
               <p className="text-sm leading-7 text-muted-foreground">
                 I'm always looking for new opportunities to learn and grow. If
-                you have any questions, please don't hesitate to reach out.
+                you have any questions, please don't hesitate to{" "}
+                <span className="text-primary">reach out.</span>
               </p>
               <div className=" flex gap-3 flex-wrap">
                 <a
